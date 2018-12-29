35aead694a29c2de8f556188d32fc871bd3024a5
Alastair Rough <rough.alastair@gmail.commit>
2018-12-29 15:16:56 -0800

    updating index.js

--- alexa/skill-sample-ferry/index.js
+++ alexa/skill-sample-ferry/index.js
@@ -31,7 +31,7 @@ const STOP_MESSAGE = 'Goodbye!';
 //=========================================================================================================================================
 //starthere
 const data = [
-'On 2018-12-29 The next ferry from Langdale will be 15:15PM. The previous ferry from Langdale at 13:05PM had above average load of 100 percent, thanks. The next ferry from Horseshoe Bay will be 16:20PM. The previous ferry from Horseshoe Bay at 14:10PM had average load of 97 percent, thanks.',];
+'On 2018-12-29 The next ferry from Langdale will be 17:25PM. The previous ferry from Langdale at 15:15PM had above average load of 92 percent, thanks. The next ferry from Horseshoe Bay will be 16:20PM. The previous ferry from Horseshoe Bay at 14:10PM had average load of 97 percent, thanks.',];
 //endhere
 
 
